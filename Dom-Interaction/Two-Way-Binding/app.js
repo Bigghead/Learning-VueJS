68792834a07f2efde45db7e126bcee8d10ac1bf9
Shav <shavenchew@yahoo.com>
2017-02-03 23:34:08 -0800

    Props/Event Emits

---
+++ Dom-Interaction/Two-Way-Binding/app.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: '#exercise',
+  data: {
+    show: true,
+    ingredients: ['meat', 'potatoes', 'food'],
+    people:[
+      {name: 'Max', age: 27},
+      {name: 'Dodo', age: 7},
+      {name: 'Kero', age: 6},
+    ]
+  }
+});
--- vue-cli/src/components/MidContent.vue
+++ vue-cli/src/components/MidContent.vue
@@ -4,15 +4,17 @@
     <div class="row">
       <div class="col-xs-6">
         <ul class='list-group'>
-          <li class='list-group-item' v-for='index in 5'>
-            Server # {{ index }}
+          <li class='list-group-item'
+              v-for='server in servers'
+              v-on:click="status = server.status">
+            Server #{{ server.id }}. Status: {{ server.status }}
           </li>
         </ul>
       </div>
 
       <div class="col-xs-6">
         <p>
-          Server Details are not currently updated
+          Server Details:{{ status }}
         </p>
       </div>
     </div>
@@ -21,6 +23,19 @@
 
 <script type="text/javascript">
   export default {
+    data: function(){
+      return {
+        servers: [
+          {id: 1, status: 'Critical'},
+          {id: 2, status: 'Normal'},
+          {id: 3, status: 'Unknown'},
+          {id: 4, status: 'Unknown'},
+          {id: 5, status: 'Critical'},
+          {id: 6, status: 'Critical'}
 
+        ],
+        status: ''
+      }
+    }
   }
 </script>
